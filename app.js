c526fe6017c426337b7a9e9953314bde66500f4d
javeria-farooq <gettingeducation6@gmail.com>
2024-01-19 13:23:48 +0500

    nested loop

---
+++ app.js
@@ -0,0 +1,13 @@
+// for(let i = 1; i<=100; i = i+10) {
+    // for(let j = i; j < i+10; j++) {
+        // document.write( j + ' ')
+    // }
+    // document.write("<br>")
+// }
+
+// for(let i = 1; i<=10000; i=i+100) {
+//     for(let j = i; j < i+100; j++) {
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
